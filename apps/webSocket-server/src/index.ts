8a5067f1f1e3d41c809cc0cedf64e9dbb794277a
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-21 22:20:38 +0530

    setup: init webSocket-server

---
+++ apps/webSocket-server/src/index.ts
@@ -0,0 +1,9 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+
+const port = 8001;
+const wss = new WebSocketServer({ port: port });
+
+wss.on("connection", (socket) => {
+  console.log("A new client is connected ", socket)
+})
