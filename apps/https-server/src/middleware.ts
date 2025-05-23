5b0ca5dc8f241d7e40062584ae2adc68677d76fc
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-23 23:47:18 +0530

    feat: authMiddleware is added for jwt verify

---
+++ apps/https-server/src/middleware.ts
@@ -0,0 +1,15 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_TOKEN } from "./config";
+import { Payload } from "./types";
+
+export const authMiddleware = async (req: any, res: any, next: any) => {
+  // const token:string = req.headers["authorization"] || "";
+  const token: string = req.headers.authorization || "";
+  const decoded = jwt.verify(token, JWT_TOKEN);
+  if (typeof decoded === "object" && decoded !== null && "userId" in decoded) {
+    req.userId = (decoded as Payload).userId;
+    next();
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
