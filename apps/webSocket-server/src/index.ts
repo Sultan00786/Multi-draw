b3f8a4c1e92b993df7312771fb58ed375533753b
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-27 22:37:10 +0530

    feat: backend-common and common is initialises

--- apps/webSocket-server/src/index.ts
+++ apps/webSocket-server/src/index.ts
@@ -9,10 +9,12 @@ wss.on("connection", (ws, req: { url: string }) => {
   const url = req.url.split("?")[1];
   const params = new URLSearchParams(url);
   const token = params.get("token");
+
   if (!token) {
     ws.close();
     return;
   }
+
   const decoded = jwt.verify(token, JWT_TOKEN);
   if (typeof decoded !== "object" || !decoded || !decoded.userId) {
     ws.close();
