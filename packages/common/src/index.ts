47064df904e120da05c82e408f613aedff9e66f6
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-29 13:13:45 +0530

    feat: zod schema added

---
+++ packages/common/src/index.ts
@@ -0,0 +1,17 @@
+import { string, z } from "zod";
+
+export const userSignUpSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  name: z.string().min(3).optional(),
+  photo: z.string().optional(),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const createRoomSchema = z.object({
+  slug: string(),
+});
