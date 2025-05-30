13080a287b484934499265aa709ea53997aa1c11
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-30 23:57:51 +0530

    fix: env undefine error which occure due to path not resolve in confit of dotenv

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,24 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import "dotenv/config";
-const db = drizzle(process.env.DATABASE_URL || "");
+import { users, rooms, chats } from "./db/schema";
+import * as schema from "./db/schema";
+import { eq } from "drizzle-orm";
+import { Pool } from "pg";
 
-export { users, rooms, chats } from "./db/schema";
+// bellow step is needed to get env variables which not required in case of prisma
+import path from "path";
+import { config } from "dotenv";
+const envPath = path.resolve(__dirname, "../.env"); // adjust if needed
+config({ path: envPath });
+
+if (!process.env.DATABASE_URL) {
+  console.log("env: ", process.env.DATABASE_URL);
+  throw new Error("DATABASE_URL not found");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for Neon
+});
+
+const db = drizzle(pool, { schema });
+export { users, rooms, chats, db, eq };
