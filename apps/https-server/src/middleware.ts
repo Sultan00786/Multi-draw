73190ff4c9567823e61154af262ede47a38a4d73
Sultan00786 <treeloversultan0987@gmail.com>
2025-07-26 12:25:44 +0530

    fix: user search in authMiddleware

--- apps/https-server/src/middleware.ts
+++ apps/https-server/src/middleware.ts
@@ -1,6 +1,7 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { Payload } from "./types";
 import { JWT_TOKEN } from "@repo/backend-common/config";
+import { db, eq, users } from "@repo/db/tables";
 
 export const authMiddleware = async (req: any, res: any, next: any) => {
   // const token:string = req.headers["authorization"] || "";
@@ -11,7 +12,15 @@ export const authMiddleware = async (req: any, res: any, next: any) => {
   }
   const decoded = jwt.verify(token, JWT_TOKEN);
   if (typeof decoded === "object" && decoded !== null && "userId" in decoded) {
-    req.userId = (decoded as Payload).userId;
+    const userId = (decoded as Payload).userId;
+    if (!userId) throw new Error();
+
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, userId),
+    });
+
+    if (!user) throw new Error();
+    req.userId = userId;
     next();
   } else {
     res.status(401).json({ error: "Unauthorized" });
